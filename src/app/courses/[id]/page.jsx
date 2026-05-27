658116b47e31fe578c0fc73fd585babee650a66f
salman <salmanibnriyaj@gmail.com>
2026-05-27 17:07:53 +0600

    retrieve jwt token from server component

--- src/app/courses/[id]/page.jsx
+++ src/app/courses/[id]/page.jsx
@@ -1,13 +1,22 @@
 import CourseDetailsCard from "@/components/CourseDetailsCard";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 
 
 const CourseDetailPage = async({params}) => {
     const {id} = await params;
     console.log(id);
 
-    
+    const {token} = await auth.api.getToken({headers: await headers()});
+    console.log(token)
 
-    const res = await fetch(`http://localhost:5001/courses/${id}`);
+
+
+    const res = await fetch(`http://localhost:5001/courses/${id}`,{
+      headers: {
+        "Authorization":`Bearer ${token}`
+      }
+    });
     const courseData = await res.json();
     console.log(courseData)
   return (
