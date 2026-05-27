8b33c3ae7b64db358983c4313bab7ad7e4efb801
salman <salmanibnriyaj@gmail.com>
2026-05-27 15:26:06 +0600

    proxy added

---
+++ src/proxy.js
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import { auth } from "./lib/auth";
+import { headers } from "next/headers";
+
+// This function can be marked `async` if using `await` inside
+export async function proxy(request) {
+  const session = await auth.api.getSession({
+    headers: await headers(), // you need to pass the headers object.
+  });
+
+  if (!session) {
+    return NextResponse.redirect(new URL("/home", request.url));
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/add-course", "/courses/:id"],
+};
